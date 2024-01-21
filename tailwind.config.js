df960a3cd99cd2f640af162d5dac59b3377c5ae0
danarseptiyanto <septiyandanar@gmail.com>
2024-01-21 07:40:56 +0700

    3

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      screens: {
+        '2xl': {'max': '1535px'},
+        // => @media (max-width: 1535px) { ... }
+  
+        'xl': {'max': '1370px'},
+        // => @media (max-width: 1279px) { ... }
+  
+        'lg': {'max': '1023px'},
+        // => @media (max-width: 1023px) { ... }
+  
+        'md': {'max': '767px'},
+        // => @media (max-width: 767px) { ... }
+  
+        'sm': {'max': '639px'},
+        // => @media (max-width: 639px) { ... }
+      },
+      fontFamily: {
+        inter: ['Inter']
+      },
+      maxWidth: {
+        'main': '1240px',
+      },
+      colors: {
+        main: "#222222",
+        secondary: "#FC4951",
+        background: "#F8F8F8",
+        stroke: "#EBEBEB",
+        drkmain: "#0B1121",
+        drksecondary: "#1E293B",
+        drktext: "#94A3B8",
+        drkborder: "#282E3F"
+      }
+    },
+  },
+}
