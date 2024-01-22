878bbc6d081bf24d1601256fec5a832b611dcec1
danarseptiyanto <septiyandanar@gmail.com>
2024-01-22 21:29:46 +0700

    aah

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ['./index.html', 'node_modules/preline/dist/*.js'],
+  content: ['./**.html', 'node_modules/preline/dist/*.js'],
   plugins: [
       require('preline/plugin'),
   ],
